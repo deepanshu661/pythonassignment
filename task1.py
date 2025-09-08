5838e1861b975e25fdfc115a8c546d4a16f07e4e
deepanshu661 <deepanshugoel224@gmail.com>
2025-09-08 16:44:10 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,15 @@
+#mathamatical operations
+a=input("enter the first number: ")
+b=input("enter the second number: ")
+a=int(a)
+b=int(b)
+num1= a
+num2= b
+addition= num1 + num2
+subtraction= num1 - num2
+multiplication= num1 * num2
+division= num1 / num2
+print("addition", addition)
+print("subtraction", subtraction)
+print("multiplication",multiplication)
+print("division",division)
---
+++ task2.py
@@ -0,0 +1,11 @@
+# Task 2: Personalized Greeting
+
+# Take first and last name as input
+a = input("Enter your first name: ")
+b = input("Enter your last name: ")
+
+# Combine first name and last name
+full_name = a + " " + b
+
+# Print the personalized greeting
+print("Hello, " + full_name + "! Welcome to the Python program")
